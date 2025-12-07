aad999a17586e4ae788c7b60eb4e9d303a80728f
Aditya Choudhary <chyaditya775@gmail.com>
2025-12-08 00:19:22 +0530

    Fix layout issue properly this time

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,8 @@ import TodoList from "../components/TodoList";
 const Home = () => {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center px-3 sm:px-4 lg:px-6">
-        <div className="flex flex-col items-center border rounded-3xl shadow-2xl max-h-screen m-2 w-full max-w-xl lg:max-w-3xl h-[88vh] sm:h-[80vh] lg:h-[88vh] overflow-hidden">
+      <div className="w-full h-screen flex flex-col items-center px-3 sm:px-4 lg:px-6">
+        <div className="flex flex-col items-center border rounded-3xl shadow-2xl max-h-screen m-2 w-full max-w-xl lg:max-w-3xl h-[90vh] sm:h-[88vh] lg:h-[88vh] overflow-hidden">
           <Title />
           <TodoInput />
           <TodoList />
